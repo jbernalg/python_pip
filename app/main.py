1141722fb09b40bcdea790e1228cba627f97b7dd
jbernalg <jeinffersonbernal@gmail.com>
2022-12-12 12:15:41 -0500

    add new library Pandas

--- app/main.py
+++ app/main.py
@@ -2,16 +2,24 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
   # obtener los datos
   data = read_csv.read_csv('data.csv')
-
+  '''
   #------------ Continente Sur America --------------------
   data_c = list(filter(lambda item: item['Continent'] == 'South America', data))
   countries = list(map(lambda x: x['Country/Territory'], data_c))
   percentages = list(map(lambda x: x['World Population Percentage'], data_c))
+  '''
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+
   charts.generate_pie_chart(countries, percentages)
 
   #------------ Un pais con la poblacion en diferentes epocas --------------
